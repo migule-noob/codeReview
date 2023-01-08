b6f7035431ba10fd0e48d7a9be62c12f3fa15cdd
migule <1634910710@qq.com>
2023-01-08 12:58:41 +0800

    v1.0.0

--- base/src/main/java/com/example/base/MainActivity.java
+++ base/src/main/java/com/example/base/MainActivity.java
@@ -1,19 +1,59 @@
 package com.example.base;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button dataBase;
+    private ActivityResultLauncher<Intent> launcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataBase = findViewById(R.id.databse);
+        dataBase.setOnClickListener(this);
+
+        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                String data = result.getData().getStringExtra("callback");
+                int resultCode = result.getResultCode();
+
+                if (RESULT_OK == resultCode) {
+                    Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.databse:{
+                Intent intent = new Intent(MainActivity.this,DataBaseActivity.class);
+                launcher.launch(intent);
+                break;
+            }
+            default:{
+                break;
+            }
+        }
     }
 }
